3ff4fbaddfef3ba0353a322cf7953e9fa4584a08
“Arian” <arian09337332108@gmail.com>
2023-11-15 10:07:18 +0330

    working on the toutorial

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,9 @@
 import requests
 
-endpoint = 'https://httpbin.org/anything'
+endpoint = 'http://127.0.0.1:8000/'
 
-get_response = requests.get(endpoint, json={'query': 'hello world'})
+get_response = requests.get(endpoint, data={'query': 'hello world'})
 
+print(get_response.text)
 print(get_response.json())
+print(get_response.status_code)
